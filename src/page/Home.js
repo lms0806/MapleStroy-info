e8daf4995f5e41e29e10e997cfa01b70156f74cd
lms0806 <godmlzkf1@naver.com>
2022-01-30 00:18:40 +0900

    제작자 표시

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import character from "../img/character.png";
 const Home = () => {
   return (
     <div>
       <h1>안녕하세요 메이플스토리 잡정보 사이트입니다.</h1>
+      <br />
+      <h2>제작자</h2>
+      <div className="Card1">
+        <img
+          className="Image"
+          alt="character"
+          src={character}
+          onClick={() =>
+            window.open(
+              "https://maple.gg/u/%EC%82%AC%EB%9E%91%EB%B2%95%EC%82%AC",
+              "_blank"
+            )
+          }
+        />
+      </div>
     </div>
   );
 };
