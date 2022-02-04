15d1fac9d448984115c5fb461a344fc260eef0c0
lms0806 <godmlzkf1@naver.com>
2022-02-04 16:14:01 +0900

    추천영상 추가

--- src/page/Video.js
+++ src/page/Video.js
@@ -8,17 +8,21 @@ const Video = () => {
     "https://youtu.be/thn1efLw7-Y",
     "https://youtu.be/rgjhkrfa_Vg",
     "https://youtu.be/xCZHnmItIiQ",
+    "https://youtu.be/1UYYJXayeQ4",
+    "https://youtu.be/LukNNXzqTwM",
   ];
   const titles = [
     "별빛 심포니 온라인 콘서트",
     "메이플스토리 세계를 확장하는 사람들, 콘텐츠 프로그래머",
     "MapleStory DESTINY Live Talk - 못다 한 이야기",
     "메이플스토리 앰배시더 | 김재덕 선수 딴짓 인터뷰",
+    "[미공개영상] 업데이트 정보센터 (요정 웡키ver) - 20211230",
+    "금손 어워즈 심사영상 [메이플스토리]",
   ];
   return (
     <div>
       <Row xs={1} md={2} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
+        {Array.from({ length: links.length }).map((_, idx) => (
           <Col>
             <Card>
               <ReactPlayer key={idx} url={links[idx]} controls />
