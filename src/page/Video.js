848802279cb98bd7de098a3811f6175062f8c4cb
lms0806 <godmlzkf1@naver.com>
2022-03-15 19:22:59 +0900

    추천 영상 추가

--- src/page/Video.js
+++ src/page/Video.js
@@ -10,6 +10,9 @@ const Video = () => {
     "https://youtu.be/xCZHnmItIiQ",
     "https://youtu.be/1UYYJXayeQ4",
     "https://youtu.be/LukNNXzqTwM",
+    "https://youtu.be/vdN38jgmbcQ",
+    "https://youtu.be/zOk8GTvF1Yg",
+    "https://youtu.be/d5MnogZ2U5Y",
   ];
   const titles = [
     "별빛 심포니 온라인 콘서트",
@@ -18,6 +21,9 @@ const Video = () => {
     "메이플스토리 앰배시더 | 김재덕 선수 딴짓 인터뷰",
     "[미공개영상] 업데이트 정보센터 (요정 웡키ver) - 20211230",
     "금손 어워즈 심사영상 [메이플스토리]",
+    "[댓글 읽기] 2022년도 다들 행복하세요! 강원기 총괄 디렉터의 새해 덕담 댓글 읽기❤",
+    "메이플 용사의 로그오프 라이프 | 블랙핑크 지수 편 - EP.01",
+    "메이플 용사의 로그오프 라이프 | 블랙핑크 지수 편 - EP.02",
   ];
   return (
     <div>
