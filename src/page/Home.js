5c12018f580e041a7072975a2b00a4f52b11e9a0
lms0806 <godmlzkf1@naver.com>
2022-03-29 15:58:55 +0900

    제작자 수정

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import character from "../img/character.png";
 import character2 from "../img/character2.png";
+import character3 from "../img/character3.png";
 
 const Home = () => {
   return (
@@ -25,6 +26,17 @@ const Home = () => {
           onClick={() =>
             window.open(
               "https://maple.gg/u/%EC%82%AC%EB%9E%91%EB%B0%94%ED%8D%BC",
+              "_blank"
+            )
+          }
+        />
+        <img
+          className="Image"
+          alt="character"
+          src={character3}
+          onClick={() =>
+            window.open(
+              "https://maple.gg/u/%EC%82%AC%EB%9E%91%EC%B2%B4%EB%9D%BC",
               "_blank"
             )
           }
