8a4be7eb2fe374d1c677178c1ff2426ef4ce0ae3
lms0806 <godmlzkf1@naver.com>
2022-07-07 22:20:17 +0900

    제작자 수정 & 모멘트리 이벤트 종료

--- src/page/Event.js
+++ src/page/Event.js
@@ -1,13 +1,10 @@
 import React from "react";
-import eventbox from "../img/eventbox.png";
 
 const event = () => {
   return (
     <div>
-      <h2>모멘트리</h2>
-      <div className="Card1">
-        <img className="Image" width="1800px" alt="character" src={eventbox} />
-      </div>
+      <h2></h2>
+      <div className="Card1"></div>
     </div>
   );
 };
